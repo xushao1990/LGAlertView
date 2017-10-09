a6ce704ba8ee8abce85f190546a2bbf235a82719
徐同同 <xushao1990@126.com>
2017-10-09 14:59:38 +0800

    去除了输入框的边距

--- LGAlertView/LGAlertViewTextField.m
+++ LGAlertView/LGAlertViewTextField.m
@@ -32,42 +32,42 @@
 
 @implementation LGAlertViewTextField
 
-- (CGRect)textRectForBounds:(CGRect)bounds {
-    bounds.origin.x += LGAlertViewPaddingWidth;
-    bounds.size.width -= LGAlertViewPaddingWidth * 2.0;
-
-    if (self.leftView) {
-        bounds.origin.x += CGRectGetWidth(self.leftView.bounds) + LGAlertViewPaddingWidth;
-        bounds.size.width -= CGRectGetWidth(self.leftView.bounds) + LGAlertViewPaddingWidth;
-    }
-
-    if (self.rightView) {
-        bounds.size.width -= CGRectGetWidth(self.rightView.bounds) + LGAlertViewPaddingWidth;
-    }
-    else if (self.clearButtonMode == UITextFieldViewModeAlways) {
-        bounds.size.width -= 20.0;
-    }
-
-    return bounds;
-}
-
-- (CGRect)editingRectForBounds:(CGRect)bounds {
-    bounds.origin.x += LGAlertViewPaddingWidth;
-    bounds.size.width -= LGAlertViewPaddingWidth * 2.0;
-
-    if (self.leftView) {
-        bounds.origin.x += CGRectGetWidth(self.leftView.bounds) + LGAlertViewPaddingWidth;
-        bounds.size.width -= CGRectGetWidth(self.leftView.bounds) + LGAlertViewPaddingWidth;
-    }
-
-    if (self.rightView) {
-        bounds.size.width -= CGRectGetWidth(self.rightView.bounds) + LGAlertViewPaddingWidth;
-    }
-    else if (self.clearButtonMode == UITextFieldViewModeAlways) {
-        bounds.size.width -= 20.0;
-    }
-
-    return bounds;
-}
+//- (CGRect)textRectForBounds:(CGRect)bounds {
+//    bounds.origin.x += LGAlertViewPaddingWidth;
+//    bounds.size.width -= LGAlertViewPaddingWidth * 2.0;
+//
+//    if (self.leftView) {
+//        bounds.origin.x += CGRectGetWidth(self.leftView.bounds) + LGAlertViewPaddingWidth;
+//        bounds.size.width -= CGRectGetWidth(self.leftView.bounds) + LGAlertViewPaddingWidth;
+//    }
+//
+//    if (self.rightView) {
+//        bounds.size.width -= CGRectGetWidth(self.rightView.bounds) + LGAlertViewPaddingWidth;
+//    }
+//    else if (self.clearButtonMode == UITextFieldViewModeAlways) {
+//        bounds.size.width -= 20.0;
+//    }
+//
+//    return bounds;
+//}
+//
+//- (CGRect)editingRectForBounds:(CGRect)bounds {
+//    bounds.origin.x += LGAlertViewPaddingWidth;
+//    bounds.size.width -= LGAlertViewPaddingWidth * 2.0;
+//
+//    if (self.leftView) {
+//        bounds.origin.x += CGRectGetWidth(self.leftView.bounds) + LGAlertViewPaddingWidth;
+//        bounds.size.width -= CGRectGetWidth(self.leftView.bounds) + LGAlertViewPaddingWidth;
+//    }
+//
+//    if (self.rightView) {
+//        bounds.size.width -= CGRectGetWidth(self.rightView.bounds) + LGAlertViewPaddingWidth;
+//    }
+//    else if (self.clearButtonMode == UITextFieldViewModeAlways) {
+//        bounds.size.width -= 20.0;
+//    }
+//
+//    return bounds;
+//}
 
 @end
